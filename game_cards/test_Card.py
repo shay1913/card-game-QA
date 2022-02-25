f4e4094a8057ef888eb17820a9cf5cc713761fa2
shay1913 <shayisso@gmail.com>
2022-02-25 18:16:47 +0200

    test __init__

--- game_cards/test_Card.py
+++ game_cards/test_Card.py
@@ -5,3 +5,15 @@ class TestCard(TestCase):
     # Testing suit and value of the card
     def setUp(self):
         self.card = Card(2,6)
+
+    # Testing valid __init__
+    def test_valid__init__(self):
+        self.assertEqual(self.card.suit,2)  # test that the suit of card valid
+        self.assertEqual(self.card.value,6)  # test that the value of card valid
+
+    # Testing invalid suit and value as str
+    def test_invalid__init__(self):
+        with self.assertRaises(TypeError):  # test suit as str
+            invalid_suit = Card("af",7)
+        with self.assertRaises(TypeError):  # test value as str
+            invalid_value = Card(1,"ggs")
