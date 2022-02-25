1d3db1322ffb270e11ed88d67c0d66e1281fc7c8
shay1913 <shayisso@gmail.com>
2022-02-25 18:40:02 +0200

    test for card_shuffle

--- game_cards/test_DeckOfCards.py
+++ game_cards/test_DeckOfCards.py
@@ -12,3 +12,9 @@ class TestDeckOfCards(TestCase):
         self.assertEqual(len(self.deck_test.all_cards),52)  # test that the list of all cards is 52 cards
         for card in self.deck_test.all_cards:
             self.assertEqual(self.deck_test.all_cards.count(card),1)  # test that the card in list of all cards only 1 time
+
+    # test that the deck of cards after shuffle is valid
+    def test_card_shuffle(self):
+        test_list1 = DeckOfCards()
+        self.deck_test.card_shuffle()
+        self.assertNotEqual(test_list1.all_cards, self.deck_test.all_cards)
