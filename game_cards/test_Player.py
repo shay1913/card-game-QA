19b020d9ab5d6c90235b29f1ebd733f1b216b92e
shay1913 <shayisso@gmail.com>
2022-02-25 18:36:57 +0200

    test for add_card

--- game_cards/test_Player.py
+++ game_cards/test_Player.py
@@ -5,12 +5,11 @@ from game_cards.Card import Card
 from game_cards.DeckOfCards import DeckOfCards
 
 
-
 class TestPlayer(TestCase):
     def setUp(self):
         self.test_player = Player("Eli")  # set the player's name valid and number of cards is default=26
 
-# ===================================================================================
+    # ===================================================================================
 
     # Test a valid case of player name in __init__
     def test_valid__init__1(self):
@@ -18,19 +17,20 @@ class TestPlayer(TestCase):
 
     # Test a valid case of num of cards in __init__
     def test_valid__init__2(self):
-        max_valid = Player("Shay",26)
-        min_valid = Player("Dani",10)
+        max_valid = Player("Shay", 26)
+        min_valid = Player("Dani", 10)
         self.assertEqual(min_valid.P_num_of_cards, 10)  # test the num of cards is 10 valid
         self.assertEqual(max_valid.P_num_of_cards, 26)  # test the num of cards is 26 valid
 
     # test if num of card bigger than 26 or lower than 10 change to default 26
     def test_valid__init__3(self):
-        roei_cards = Player("Roei",43)
-        yael_cards = Player("Yael",4)
+        roei_cards = Player("Roei", 43)
+        yael_cards = Player("Yael", 4)
         self.assertEqual(roei_cards.P_num_of_cards, 26)
         self.assertEqual(yael_cards.P_num_of_cards, 26)
 
         # Test invalid parameters sent to __init__
+
     def test_invalid__init__(self):
         # invalid name
         with self.assertRaises(TypeError):
@@ -39,32 +39,55 @@ class TestPlayer(TestCase):
         with self.assertRaises(TypeError):
             invalid_test_player2 = Player("Shay", "abc")  # test that num of cards can't be str!
 
-# ===================================================================================
+    # ===================================================================================
 
     # Test that set_hand is valid and divide cards to players
     def test_set_hand(self):
         deck = DeckOfCards()
-        self.assertEqual(len(self.test_player.P_cards),0)
+        self.assertEqual(len(self.test_player.P_cards), 0)
         self.test_player.set_hand(deck)
-        self.assertEqual(len(self.test_player.P_cards),26)
+        self.assertEqual(len(self.test_player.P_cards), 26)
 
     # Checks that the dealt card from the deck is in Player's hand
-    @mock.patch('game_cards.DeckOfCards.DeckOfCards.deal_one',return_value = Card(2,5))
-    def test_set_hand2(self,mock_deal_one):
-        player = Player("Yoni",10)
+    @mock.patch('game_cards.DeckOfCards.DeckOfCards.deal_one', return_value=Card(2, 5))
+    def test_set_hand2(self, mock_deal_one):
+        player = Player("Yoni", 10)
         deck = DeckOfCards()
         player.set_hand(deck)
-        self.assertEqual(player.P_cards.count(Card(2,5)),10)  # Check that the dealt card is in Player's hand
+        self.assertEqual(player.P_cards.count(Card(2, 5)), 10)  # Check that the dealt card is in Player's hand
 
-# ===================================================================================
+    # ===================================================================================
 
     # Test that random card pulls out from player's deck of cards
     def test_get_card(self):
         deck = DeckOfCards()
         self.test_player.set_hand(deck)
-        self.assertEqual(len(self.test_player.P_cards),26)
+        self.assertEqual(len(self.test_player.P_cards), 26)
         self.test_player.get_card()
-        self.assertEqual(len(self.test_player.P_cards),25)
+        self.assertEqual(len(self.test_player.P_cards), 25)
 
-# ===================================================================================
+    # ===================================================================================
 
+    # Test that add_card adding a card to the player list
+    def test_valid_add_card(self):
+        deck = DeckOfCards()
+        card = Card(2, 6)
+        self.test_player.set_hand(deck)
+        self.assertEqual(len(self.test_player.P_cards), 26)
+        self.test_player.add_card(card)
+        self.assertEqual(len(self.test_player.P_cards), 27)
+
+    # Test that the add_card adding the specific card to the list
+    def test_valid_add_card2(self):
+        deck = DeckOfCards()
+        card = Card(2, 6)
+        self.test_player.set_hand(deck)
+        self.assertNotIn(card, self.test_player.P_cards)
+        self.test_player.add_card(card)
+        self.assertIn(card, self.test_player.P_cards)
+
+    # Test that the card type must be Card in addition to add him
+    def test_invalid_add_card(self):
+        with self.assertRaises(TypeError):
+            invalid_card =  12,"afaf"
+            self.test_player.add_card(invalid_card)
