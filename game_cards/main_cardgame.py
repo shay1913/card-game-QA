e099c102a3e2feb424a22f60b1bdf38fea6bc27a
shay1913 <shayisso@gmail.com>
2022-02-25 14:38:54 +0200

    main program

--- game_cards/main_cardgame.py
+++ game_cards/main_cardgame.py
@@ -8,3 +8,14 @@ card_deck = DeckOfCards()
 war_game = CardGame(player1,player2,26)
 print(f"{player1},\n{war_game.player1.P_cards}")
 print(f"{player2},\n{war_game.player2.P_cards}")
+
+for i in range(10):
+    card1 = war_game.player1.get_card()
+    card2 = war_game.player2.get_card()
+
+    if card1 > card2:
+        war_game.player1.add_card(card1)
+        war_game.player1.add_card(card2)
+        print(f"{player1}:{card1}")
+        print(f"{player2}:{card2}")
+        print(f"{player1} win the round")
